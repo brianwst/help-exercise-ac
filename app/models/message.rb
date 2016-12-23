7e2a9bc28f0a667d901f81c15f2dcd7c1dbfbb8f
Brian Wong <brian.wongst@gmail.com>
2016-12-23 15:32:01 +0800

    Add scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
 
   has_many :comments, :dependent => :destroy
+  scope :pending, -> {where( :status => "pending" )}
+  scope :completed, -> {where( :status => "completed" )}
+  scope :within_days, ->(day) {where( ["created_at >= ?", Time.now - day.days ] )}
 
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
